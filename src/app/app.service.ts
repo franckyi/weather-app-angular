c5f3e0020c669f09b8c077e693f6866a7de9c9ed
Francky Iside <francky@tuta.io>
2023-03-16 23:30:35 +0100

    add service to handle api calls

---
+++ src/app/app.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppService {
+
+  constructor(private _httpClient: HttpClient) { }
+
+  getLocalWeather(lat: Number, lon: Number) {
+    return this._httpClient.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=61a20f5d41830810abfcc3d15f5f1b2a&units=metric`);
+  }
+
+  if (navigator.geolocation) {
+    const getCoords = () => {
+      navigator.geolocation.getCurrentPosition( position => {
+          let lat = position.coords.latitude;
+          let lon = position.coords.longitude;
+          this.getLocalWeather(lat, lon)
+      })
+    }
+  }
+
+}
