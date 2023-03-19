055fc37977a8939d652883d0700f166c71bc199a
Francky Iside <francky@tuta.io>
2023-03-19 10:38:24 +0100

    get local weather

--- src/app/features/current-weather/weather/weather.component.ts
+++ src/app/features/current-weather/weather/weather.component.ts
@@ -13,40 +13,31 @@ export class WeatherComponent implements OnInit {
 
   constructor(private _weatherHandlerService: WeatherHandlerService) { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit() {
+    this.getCoords ()  }
 
   getCoords () {
+    console.warn('called getCoords()')
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition( position => {
         let lat = position.coords.latitude;
         let lon = position.coords.longitude;
         this._weatherHandlerService.getLocalWeather(lat, lon)
-        .subscribe((data: CurrentWeatherResponse) => this.current = {
-          lon: data.coord.lon,
-          lat: data.coord.lat,
-          description: data.weather[0].description,
-          icon: data.weather[0].icon,
-          temp: data.main.temp,
-          feels_like: data.main.feels_like,
-          temp_min: data.main.temp_min,
-          temp_max: data.main.temp_max,
-          pressure: data.main.pressure,
-          humidity: data.main.humidity,
-          visibility: data.visibility,
-          speed: data.wind.speed,
-          deg: data.wind.deg,
-          all: data.clouds.all,
-          dt: data.dt,
-          country: data.sys.country,
-          sunrise: data.sys.sunrise,
-          sunset: data.sys.sunset,
-          timezone: data.timezone,
-          name: data.name,
-          // ...data
-      });
+        .subscribe(
+          (response) => {                           //next() callback
+            console.warn('✅ response received')
+            this.current = response;
+            console.log('current', this.current)
+          }
+        )
+
+
       })
     }
+
+
   }
+
+
 
 }
